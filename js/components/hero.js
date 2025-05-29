0eee15f3857ba90d96fa411330b9cbee738a1032
Slim-Solo <artur.pucnin@gmail.com>
2025-05-29 11:10:56 +0300

    plus font and home

--- js/components/hero.js
+++ js/components/hero.js
@@ -5,15 +5,14 @@ export function hero() {
         <div class="container">
             <div class="row">
                 <div class="col-12 col-lg-6 m-lg-3">
-                    <div>Our Service</div>
-                    <h2>Electricity Service Offerings</h2>
-                    <p>From residential households to commercial enterprises, we provide reliable, efficient, and sustainable energy solutions.</p>
+                    <div>Streaming your Workflow</div>
+                    <h1>Organize, Track, and Complete Task Efficiently</h1>
+                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or</p>
                 </div>
             </div>
             <div class="row">
-                <div class="col-12 col-md-6 col-lg-4">CARD</div>
-                <div class="col-12 col-md-6 col-lg-4">CARD</div>
-                <div class="col-12 col-md-6 col-lg-4">CARD</div>
+                <div class="col-12 col-md-6 col-lg-4"><button>Get Started Now</button></div>
+                <div class="col-12 col-md-6 col-lg-4"><button>Download App</button></div>
             </div>
         </div>
         `;
