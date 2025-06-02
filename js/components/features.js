268fc378fb55f0575b93c2d07f3c59b8e3f855fe
webz <zilvism@gmail.com>
2025-06-02 09:19:28 +0300

    corrections

--- js/components/features.js
+++ js/components/features.js
@@ -3,10 +3,10 @@ export function features() {
  `<div class="container">
         <div class="row">
         <img class="col-12 col-lg-6" src="../img/stats2.png">
-        <div class="col-12 col-lg-5"> 
+        <div class="col-12 col-lg-5">
         <div class="section-tag">Our Features</div>
-        <h1>We Provide The Best Quality</h1>
-        <p>There are many variations of passages of Lorem Ipsum available, 
+        <h1 style="font-size:38px;">We Provide The Best Quality</h1>
+        <p style="color:rgb(187, 187, 187)">There are many variations of passages of Lorem Ipsum available, 
         but the majority have suffered alteration in some form, by injected humour, 
         or randomised words which don't look even slightly</p>      
         <ul>
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,7 +1,6 @@
 import { header } from "../components/header.js";
 import { hero } from "../components/hero.js";
 import { features } from "../components/features.js";
- 
 
 header();
 hero();
